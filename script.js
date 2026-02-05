55083a778a574247fb27daab25a88bfee6605edd
V1tteba <gabrielguerra3366@gmail.com>
2026-02-04 23:27:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const porta = document.getElementById("porta");
+const cama = document.getElementById("cama");
+const janela = document.getElementById("janela");
+const espelho = document.getElementById("espelho");
+porta.addEventListener("click", () => {
+    window.location.href = "https://v1tteba.neocities.org"; 
+});
+cama.addEventListener("click", () => {
+    window.location.href = "bed.html"; 
+});
+espelho.addEventListener("click", () => {
+    window.location.href = "itsme.html"; 
+});
+janela.addEventListener("click", () => {
+    window.location.href = "window.html"; 
+});
